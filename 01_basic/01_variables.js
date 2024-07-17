fe18869db2088d52604638e837f6a679aa688417
tanujapatil208 <101786206+tanujapatil208@users.noreply.github.com>
2024-07-17 09:45:02 +0000

    variables

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,8 +1,14 @@
 const accountId = 1234567
-let acoountEmail = "Tanuja@0000google.com"
+let accountEmail = "Tanuja@0000google.com"
 var accountPassword = "121212"
 accointCity = "Latur"
+//above is also variable//
+let accountState;
 
-accountId = 2
+// accountId = 2 //
 
 console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accointCity,accountState])
+
+
+/*prefer not to use var */
