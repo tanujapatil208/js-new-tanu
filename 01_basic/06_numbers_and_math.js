9fc311f06b73fb8c5df1e078b816b4b5e828aed6
tanujapatil208 <101786206+tanujapatil208@users.noreply.github.com>
2024-07-24 07:43:00 +0000

    numbers and maths

---
+++ 01_basic/06_numbers_and_math.js
@@ -0,0 +1,29 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1)); // points nanther kiti value ghyacha te sangto apan.(100.122 = 2 fix value bol apan tr (100.12) yeil) 
+
+const otherNumber = 123.8966
+
+//console.log(otherNumber.toPrecision(3)); // in this we make roundoff value ex:- 123.899 = 124
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN')); // indain value madhe convert karat.
+
+
+// ******************* Maths******************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));// -ive values converts into +ive values.
+// console.log(Math.round(4.7));// roundoff det values ex:- 4.3 = 4, 5.6 = 6.
+// console.log(Math.ceil(4.2)); // Mothi value roundoff kart ex:- 4.2= 5, 5,1= 6.
+// console.log(Math.floor(4.9));// Lahan value roundoff kart ex:- 4.9= 4, 5.9= 5.
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(3,4,5,6));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
