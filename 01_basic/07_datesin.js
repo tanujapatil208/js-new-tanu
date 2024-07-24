f604aa0b8ddb00ee58c78dbfec499e145ea1401a
tanujapatil208 <101786206+tanujapatil208@users.noreply.github.com>
2024-07-24 08:20:45 +0000

    datees

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,13 @@
+// Dates 
+
+let myDate = new Date()
+console.log(myDate.toString());      //wed Jul 24 2024 08:00:58 GMT+0000 :-day/month/date/year/time
+console.log(myDate.toDateString());   // wed/ jul / 24 / 2024 :- day/month/date/year
+console.log(myDate.toISOString());     // 2024-07-24T07 :- year/month/date
+console.log(myDate.toLocaleDateString());    //7/24/2024  :- month/date/year
+
+console.log(typeof myDate);  //object.
+
+let myCreatedDate = new Date(2024, 6, 24, 1, 39) // mm/dd/yy/Time :- 7/24.2024, 1:39
+//let myCreatedDate = new Date("2024-07-24") // yy/mm/dd :- 7/24/2024
+console.log(myCreatedDate.toLocaleString());  
