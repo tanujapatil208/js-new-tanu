38fc82ed0023c9e59aa39f87acca2938bdbd7f7c
tanujapatil208 <101786206+tanujapatil208@users.noreply.github.com>
2024-07-26 09:26:15 +0000

    dates and time.js

--- 01_basic/07_datesin.js
+++ 01_basic/07_datesin.js
@@ -8,6 +8,24 @@ console.log(myDate.toLocaleDateString());    //7/24/2024  :- month/date/year
 
 console.log(typeof myDate);  //object.
 
-let myCreatedDate = new Date(2024, 6, 24, 1, 39) // mm/dd/yy/Time :- 7/24.2024, 1:39
-//let myCreatedDate = new Date("2024-07-24") // yy/mm/dd :- 7/24/2024
+//let myCreatedDate = new Date(2024, 6, 24, 1, 39) // mm/dd/yy/Time :- 7/24.2024, 1:39
+let myCreatedDate = new Date("2024-07-24") // yy/mm/dd :- 7/24/2024
 console.log(myCreatedDate.toLocaleString());  
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));// atachi date deta (in milisecond)
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
+
