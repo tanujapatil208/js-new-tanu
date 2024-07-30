e0d98d58dd58f0ad634078a763d9aeadd4c5e516
tanujapatil208 <101786206+tanujapatil208@users.noreply.github.com>
2024-07-30 08:14:46 +0000

    arrays.js

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -24,15 +24,16 @@ const newArr = myArr.join()
 console.log(myArr);
 console.log(typeof newArr);
 
-//slice , splice
+//slice 
+//splice
 
 console.log("A ",myArr);
 
-const myn1 = myArr.slice(1,3)
+const myn1 = myArr.slice(1,4) // cut the value from array[0,1,2,3,4,5]:- slice(0,4)("0 to value-1"):-[1,2,3]
 
 console.log(myn1);
 console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1,4) // cut the value from array[0,1,2,3,4,5]:- splic2(1,4):-[0,5] 
 console.log("C ",myArr);
 console.log(myn2);
