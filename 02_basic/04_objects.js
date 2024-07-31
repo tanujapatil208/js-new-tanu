408de070607bca67af08a8a8dea729fcdc29f693
tanujapatil208 <101786206+tanujapatil208@users.noreply.github.com>
2024-07-31 08:58:38 +0000

    objects.js

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,54 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+
+ tinderUser.id = "123abc"
+ tinderUser.name= "Tanuja"
+ tinderUser.isLoggedIn = false
+ 
+ console.log(tinderUser);
+
+ const regularUser = {
+    email: "patiltanuja@207.com",
+    fullname: {       //one bject into another object (in Loop).
+        userfullname: {
+        firstname: "Tanuja",
+        lastname: "Patil"   
+       }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3);   // this will only combines the two objects.
+
+//const obj3 = Object.assign({},obj1,obj2,obj4) // marage 2{}{} objects into 1 objects{... ... } 
+
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+const users =[
+    {
+        id:1,
+        email: "patiltanuja@2364.com"
+    },
+    {
+        id:1,
+        email: "patiltanuja@2364.com"
+    },
+    {
+        id:1,
+        email: "patiltanuja@2364.com"
+    },
+    
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnPropperty('isLoggedIn'));
