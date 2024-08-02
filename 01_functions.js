7c5ac783d872c7376009492e0751a00a23d719eb
tanujapatil208 <101786206+tanujapatil208@users.noreply.github.com>
2024-08-02 09:52:12 +0000

    functins.js

--- 01_functions.js
+++ 01_functions.js
@@ -36,3 +36,40 @@ function loginUserMessage(username = "Patil"){ // jar value undefined asel tr ap
 }
 console.log(loginUserMessage("Tanuja"))
 console.log(loginUserMessage()); // value pass kel mahi tr undefined yenae.
+
+
+//************************
+
+
+//ANOTHER VIDEO//
+
+
+ //FUNCTION
+                                                                                            //      1:-200      2:-400      500
+function calculaateCartPrice(vall1,vall2, ...num1){ // (num1):-200 ,   (...num1):- 200,400,500 , (vall1,vall2...num1):- 500.
+    return num1
+}
+console.log(calculaateCartPrice(200,400,500));
+
+const user ={
+    username:"Tanuja",     
+    price: 199
+}
+
+function handleobject(anyobject){    
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleobject({                // direct objects pass
+    username:"Patil",        //objects write as shown
+    price:200
+})
+
+const myNewArray = [200,400,600,100]      // arrays objects pass
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,600,100]));
+
+
